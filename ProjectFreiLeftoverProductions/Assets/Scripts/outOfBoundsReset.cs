12b0b07df319872ddffc88c51489917b90168020
JHunter465 <jael.vanrossum@student.hku.nl>
2019-06-18 14:27:19 +0200

    Shaders Gekut

---
+++ ProjectFreiLeftoverProductions/Assets/Scripts/outOfBoundsReset.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class outOfBoundsReset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.GetComponent<InteractibleItem>())
+    //    {
+    //        other.GetComponent<InteractibleItem>().Resetpos();
+    //    }
+    //}
+}
