41ce0fd4a6e5ad29e8944ba5f0476b6d53e6389a
Casper van Battum <cvanbattum@gmail.com>
2019-06-25 09:40:16 +0200

    Car controlled by trigger

--- ProjectFreiLeftoverProductions/Assets/Scripts/CarControls.cs
+++ ProjectFreiLeftoverProductions/Assets/Scripts/CarControls.cs
@@ -1,8 +1,18 @@
 ﻿using UnityEngine;
+using Valve.VR;
 
 [RequireComponent(typeof(Car))]
 public class CarControls : MonoBehaviour {
 	private Car car;
+
+	public SteamVR_Input_Sources handType;        // 1
+	public SteamVR_Action_Boolean teleportAction; // 2
+	public SteamVR_Action_Boolean grabAction;     // 3
+
+	public bool GetGrab() // 2
+	{
+		return grabAction.GetState(handType);
+	}
 
 	private void Start() {
 		car = GetComponent<Car>();
@@ -10,6 +20,10 @@ public class CarControls : MonoBehaviour {
 
 	private void FixedUpdate() {
 		// TODO replace with vr controller trigger as scaling value?
+		if (GetGrab()) {
+			Debug.Log("this shit works");
+		}
+
 		if (Input.GetKey(KeyCode.Space)) {
 			car.Accelerate();
 		}
